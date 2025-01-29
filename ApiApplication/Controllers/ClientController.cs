e1c5ea6e8b5baf8d330203e9c55d3682582892ff
RAZANADAHY <mandresyshz@gmail.com>
2025-01-29 22:07:35 +0300

    29/01/2025 22:06
    don't use Dto for insert

--- ApiApplication/Controllers/ClientController.cs
+++ ApiApplication/Controllers/ClientController.cs
@@ -23,6 +23,7 @@ namespace ApiApplication.Controllers
             return Ok(_dbContext.Clients.ToList());
         }
 
+
         [HttpGet("{id}")]
         public IActionResult GetById(int id)
         {
@@ -35,24 +36,43 @@ namespace ApiApplication.Controllers
         }
 
 
-        [HttpPost]
-        public IActionResult Post(Insert_Client_Dto clientDto)
-        {
-            var client = new Client
-            {
-                Nom = clientDto.Nom,
-                Prenom = clientDto.Prenom,
-                Adresse = clientDto.Adresse,
-                Ville = clientDto.Ville,
-                CodePostal = clientDto.CodePostal,
-                Telephone = clientDto.Telephone,
-                Email = clientDto.Email
-            };
+        //[HttpPost]
+        //public IActionResult Post(Insert_Client_Dto clientDto)
+        //{
+        //    var client = new Client
+        //    {
+        //        Nom = clientDto.Nom,
+        //        Prenom = clientDto.Prenom,
+        //        Adresse = clientDto.Adresse,
+        //        Ville = clientDto.Ville,
+        //        CodePostal = clientDto.CodePostal,
+        //        Telephone = clientDto.Telephone,
+        //        Email = clientDto.Email
+        //    };
 
+        //    _dbContext.Clients.Add(client);
+        //    _dbContext.SaveChanges();
+        //    return Ok(client);
+        //}
+
+
+        //[HttpPost]
+        //public async Task<IActionResult> Post(Client client)
+        //{
+        //    _dbContext.Clients.Add(client);
+        //    await _dbContext.SaveChangesAsync();
+        //    return Ok(client);
+        //}
+
+
+        [HttpPost]
+        public IActionResult Post(Client client)
+        {
             _dbContext.Clients.Add(client);
             _dbContext.SaveChanges();
             return Ok(client);
         }
+
 
     }
 
--- ApiApplication/obj/Debug/net8.0/ApiApplication.AssemblyInfo.cs
+++ ApiApplication/obj/Debug/net8.0/ApiApplication.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApiApplication")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7efe0d30de4778f79b2f982063bfb171f68d092")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApiApplication")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ApiApplication")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
